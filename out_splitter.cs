6cf4b65ede15231db13e7a46e8beef2faee564de
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-14 16:29:00 -0700

    Added writers vector

--- out_splitter.cs
+++ out_splitter.cs
@@ -3,8 +3,10 @@ using System.Text;
 
 class TextOutSplitter : TextWriter {
     public override Encoding Encoding => new System.Text.UTF8Encoding();
+    private IReadOnlyCollection<TextWriter> writers;
 
-    TextOutSplitter() {
+    TextOutSplitter(IReadOnlyCollection<TextWriter> writers) {
+        this.writers = writers;
     }
     
     public void write(char c) {
