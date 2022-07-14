27a0775dff4ef1d85b69bb8fbd56d766bd38e142
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-14 11:54:23 -0700

    Basic TextWriter splitting framework

---
+++ out_splitter.cs
@@ -0,0 +1,10 @@
+
+class TextOutSplitter : TextWriter {
+    TextOutSplitter() {
+        this.Encoding = new System.Text.UTF8Encoding();
+    }
+    
+    public void write(char c) {
+        // TODO
+    }
+}
