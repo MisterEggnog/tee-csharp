54ad3c0fa77424ee9c00a8c1b8182e9742b12e38
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-14 20:56:00 -0700

    Test idea, no ref to source yet

--- tee-splitter.test/out-splitter-test.cs
+++ tee-splitter.test/out-splitter-test.cs
@@ -3,8 +3,16 @@ namespace tee_splitter.test;
 public class OutSplitterTest
 {
     [Fact]
-    public void Test1()
+    public void basic_char_multiwrite()
     {
+        const string test_str = "My hovercraft is full of eels. huegh";
+        var string_writers = new List<StringWriter>();
+        for (int i = 0; i < 10; i++) {
+            string_writers.Add(new StringWriter());
+        }
 
+        foreach (var s in string_writers) {
+            Assert.Equal(test_str, s.ToString());
+        }
     }
 }
