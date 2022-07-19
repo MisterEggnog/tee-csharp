4328bf3faaf7e8ecfc8678bfabf9e0a8631b9648
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-18 22:09:59 -0700

    Implemented ReaderDisposeChecker
    If TextWritier/TextReader were interfaces I could just create 1 class
    with 1 implementation of IDisposable rather than having to copy code.

--- tee-splitter.test/dispose-checkers.cs
+++ tee-splitter.test/dispose-checkers.cs
@@ -1,6 +1,7 @@
 namespace tee_splitter.test;
 
-// If I could I would derive this from the null TextWriter.
+// If I could I would derive these from the null Writers/Readers.
+
 public class WriterDisposeChecker : StringWriter {
     public bool has_been_disposed { get; set; }
 
@@ -12,6 +13,22 @@ public class WriterDisposeChecker : StringWriter {
     [Fact]
     public void dispose_checker_sets_bool() {
         var dispose_checker = new WriterDisposeChecker();
+        Assert.False(dispose_checker.has_been_disposed);
+        dispose_checker.Dispose();
+        Assert.True(dispose_checker.has_been_disposed);
+    }
+}
+
+public class ReaderDisposeChecker: TextReader {
+    public bool has_been_disposed { get; set; }
+
+    protected override void Dispose(bool disposing) {
+        this.has_been_disposed = disposing;
+    }
+
+    [Fact]
+    public void dispose_checker_sets_bool() {
+        var dispose_checker = new ReaderDisposeChecker();
         Assert.False(dispose_checker.has_been_disposed);
         dispose_checker.Dispose();
         Assert.True(dispose_checker.has_been_disposed);
