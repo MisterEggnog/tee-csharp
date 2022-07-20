8512a44e395c96a430385ad69d01119dcb2cc450
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-19 19:03:08 -0700

    Unit test for Tee

--- tee-splitter.test/tee.test.cs
+++ tee-splitter.test/tee.test.cs
@@ -1,4 +1,16 @@
+namespace tee_splitter.test;
 
-class TeeTest {
-    
+public class TeeTest {
+    [Fact]
+    public void tee_input_output_checking() {
+        const String test_str = "Waku waku.\nEEEEE\nRawr\n\n";
+        var str_in = new StringReader(test_str);
+        var str_out = new StringWriter();
+        Console.SetIn(str_in);
+        Console.SetOut(str_out);
+
+        Tee.run(new List<String>());
+
+        Assert.Equal(test_str, str_out.ToString());
+    }
 }
