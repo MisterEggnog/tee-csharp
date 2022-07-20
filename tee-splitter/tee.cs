eb376bc8d4b56ee718c05136693fc0fc58d86147
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-20 14:33:42 -0700

    Tee handles sdin->stdout

--- tee-splitter/tee.cs
+++ tee-splitter/tee.cs
@@ -1,6 +1,19 @@
 
 public class Tee {
+
+    static List<TextWriter> open_files(IReadOnlyCollection<String> files) {
+        // TODO
+        return new List<TextWriter>();
+    }
+
     public static int run(IReadOnlyCollection<String> files) {
-        throw new NotImplementedException();
+        var open_files = Tee.open_files(files);
+        open_files.Add(Console.Out);
+        var output_splitter = new TextOutSplitter(open_files);
+        var pipe = new TextTransferPipe(Console.In, output_splitter);
+        pipe.transfer();
+        pipe.Dispose();
+
+        return 0;
     }
 }
