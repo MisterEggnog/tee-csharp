da560555b309d4d570b844b9cda8974d93d6de56
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-23 22:43:07 -0700

    Main functions

--- tee-splitter/Program.cs
+++ tee-splitter/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var files = new List<String>();
+var result = Tee.run(files);
+return result;
