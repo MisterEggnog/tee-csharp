acce9d2c6a25f7590dac3ed0699fb63ad87a7631
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-25 22:12:55 -0700

    Implemented file list parsing

--- tee-splitter/args_parser.cs
+++ tee-splitter/args_parser.cs
@@ -3,6 +3,6 @@ public class ArgsParser {
     public readonly IReadOnlyList<String> files;
 
     public ArgsParser(IReadOnlyList<String> args) {
-        files = new List<String>();
+        files = args;
     }
 }
