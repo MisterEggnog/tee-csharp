915f11f652bf1796f79682f88f8ab47aef4b3cd0
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-25 22:46:46 -0700

    Worked on argument parser constructor

--- tee-splitter/args_parser.cs
+++ tee-splitter/args_parser.cs
@@ -5,6 +5,39 @@ public class ArgsParser {
     public readonly bool ignore_signals;
 
     public ArgsParser(IReadOnlyList<String> args) {
-        files = args;
+        var files = new List<String>();
+        bool dash_dash = false;
+
+        foreach (var arg in args) {
+            if (!dash_dash) {
+                if (arg[0] == '-') {
+                    foreach (var c in arg) {
+                        if (c == 'a')
+                            this.append = true;
+                        else if (c == 'i')
+                            this.ignore_signals = true;
+                        else if (c == '-')
+                            // Hack
+                            dash_dash = true;
+                        else
+                            throw new InvalidArgument($"{c} is not a valid argument switch.");
+                    }
+                } else {
+                    files.Add(arg);
+                }
+            } else {
+                files.Add(arg);
+            }
+        }
+
+        this.files = files;
+    }
+}
+
+public class InvalidArgument: Exception {
+    readonly string wrong_argument;
+    
+    public InvalidArgument(string msg) {
+        this.wrong_argument = msg;
     }
 }
