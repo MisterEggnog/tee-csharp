03bed1bdaa6c84cc4f8d6c359de959c99f598b99
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-25 23:26:18 -0700

    InvalidArgument readonly public

--- tee-splitter/args_parser.cs
+++ tee-splitter/args_parser.cs
@@ -41,7 +41,7 @@ public class ArgsParser {
 }
 
 public class InvalidArgument: Exception {
-    readonly string wrong_argument;
+    public readonly string wrong_argument;
     
     public InvalidArgument(char c) {
         this.wrong_argument = $"{c} is not a valid argument switch.";
