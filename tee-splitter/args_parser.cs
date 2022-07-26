d81fe5ce84577ead6186e22c0953d4f2ec19b4dc
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-25 22:15:07 -0700

    Added other switches to arg parser

--- tee-splitter/args_parser.cs
+++ tee-splitter/args_parser.cs
@@ -1,6 +1,8 @@
 
 public class ArgsParser {
     public readonly IReadOnlyList<String> files;
+    public readonly bool append;
+    public readonly bool ignore_signals;
 
     public ArgsParser(IReadOnlyList<String> args) {
         files = args;
