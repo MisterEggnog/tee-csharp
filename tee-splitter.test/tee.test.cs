81a7399aa2e26cb3aa041938ec25476b934db670
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-26 22:37:42 -0700

    Use only 1 test string in TeeTest

--- tee-splitter.test/tee.test.cs
+++ tee-splitter.test/tee.test.cs
@@ -7,7 +7,6 @@ public class TeeTest {
 
     [Fact]
     public void tee_input_output_checking() {
-        const String test_str = "Waku waku.\nEEEEE\nRawr\n\n";
         var str_in = new StringReader(test_str);
         var str_out = new StringWriter();
         Console.SetIn(str_in);
