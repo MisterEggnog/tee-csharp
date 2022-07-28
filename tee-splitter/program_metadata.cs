43898d114fcb28bb45127c3b4319f0f9ff2e8885
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-28 13:21:05 -0700

    Wrote license str for some reason

--- tee-splitter/program_metadata.cs
+++ tee-splitter/program_metadata.cs
@@ -10,4 +10,16 @@ public class ProgramMetadata {
             + "Licensed under GPL V3, use --license for more info";
         return msg;
     }
+
+    public String license_str() {
+        var msg =
+            "Copyright (C) 2022 Josiah Baldwin\n" +
+            "This program comes with ABSOLUTELY NO WARRANTY; for more info see the gnu V3 license.\n" +
+            "This is free software, and you are welcome to redistribute it\n" +
+            "under certain conditions; for more info see the gnu V3 license.\n" +
+            "\n" +
+            "You should have received a copy of the GNU General Public License\n" +
+            "along with this program.  If not, see <https://www.gnu.org/licenses/>.";
+        return msg;
+    }
 }
